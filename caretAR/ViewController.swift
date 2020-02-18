7f2b05c4e13b536aaf187f9199014866a9262cef
Galdineris <galdineris@gmail.com>
2020-02-18 16:07:29 -0300

    Initial Commit

---
+++ caretAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  caretAR
+//
+//  Created by Rafael Galdino on 18/02/20.
+//  Copyright © 2020 Rafael Galdino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
